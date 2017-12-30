66a474fe56bf1e8145d7992eac70b814c9025324
RobinJason <r.jase@live.com>
2017-12-30 22:11:54 +0800

    change the code to the first step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
 })
 /*添加属性,title属性用来表示应用的名字，hero属性用来表示叫做windstorm的英雄*/
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  hero = 'Windstorm';
 }
