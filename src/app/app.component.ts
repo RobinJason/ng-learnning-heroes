906217c66e9b9c33bd5c7703abd3172ea2034d0f
RobinJason <r.jase@live.com>
2018-01-01 14:50:30 +0800

    step 3: add more details about the hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,12 @@ export class Hero {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label>{{hero.name}}</div>
+  `
 })
 /*添加属性,title属性用来表示应用的名字，hero属性用来表示叫做windstorm的英雄*/
 export class AppComponent {
