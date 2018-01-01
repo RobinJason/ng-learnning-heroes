de8fe6f6f5fc6416f3d2fc142f35a376c5d1a0d1
RobinJason <r.jase@live.com>
2018-01-01 14:47:23 +0800

    change hero strings to a Hero object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-
+/*Hero类*/
+export class Hero {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+  template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
 /*添加属性,title属性用来表示应用的名字，hero属性用来表示叫做windstorm的英雄*/
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
